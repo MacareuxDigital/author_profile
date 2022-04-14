f9c3cc88a60a0abee7ba790b4387993814de64a4
aye <aye@macareux.co.jp>
2022-04-14 10:35:12 +0900

    Atomikテーマの場ブロックが編集できないのでCSS修正

--- blocks/author_page_list/page_list_form.php
+++ blocks/author_page_list/page_list_form.php
@@ -276,7 +276,6 @@ $noResultsMessage = $noResultsMessage ?? null;
     div.authorpagelist-form div.cover {
         width: 100%;
         height: 100%;
-        position: absolute;
         top: 0;
         left: 0;
     }
